37f2b2789fa90d323996cdd499cb40172b1e4494
Minjee Kim <mjkim9001@gmail.com>
2020-07-27 17:47:26 +0900

    20200727

--- Project/app.py
+++ Project/app.py
@@ -1,38 +1,155 @@
 from flask import Flask, render_template, request
+from keras.applications.vgg16 import VGG16, decode_predictions
+from werkzeug import secure_filename
+import os
+import re
+import joblib
+import numpy as np
+import pandas as pd
+from PIL import Image
+from konlpy.tag import Okt
+from tensorflow import keras
+from keras.models import load_model
+
 app = Flask(__name__)
+app.debug = True
+
+
+okt = Okt()
+movie_lr = None
+movie_lr_dtm = None
+def load_movie_lr():
+    global movie_lr, movie_lr_dtm
+    movie_lr = joblib.load(os.path.join(app.root_path, 'model/movie_lr.pkl'))
+    movie_lr_dtm = joblib.load(os.path.join(app.root_path, 'model/movie_lr_dtm.pkl'))
+
+def tw_tokenizer(text):
+    # 입력 인자로 들어온 text 를 형태소 단어로 토큰화 하여 list 객체 반환
+    tokens_ko = okt.morphs(text)
+    return tokens_ko
+
+movie_nb = None
+movie_nb_dtm = None
+def load_movie_nb():
+    global movie_nb, movie_nb_dtm
+    movie_nb = joblib.load(os.path.join(app.root_path, 'model/movie_nb.pkl'))
+    movie_nb_dtm = joblib.load(os.path.join(app.root_path, 'model/movie_nb_dtm.pkl'))
+
+def nb_transform(review):
+    stopwords=['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
+    review = review.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+    morphs = okt.morphs(review, stem=True) # 토큰화
+    temp = ' '.join(morph for morph in morphs if not morph in stopwords)
+    return temp
+
+def load_vgg():
+    global vgg
+    vgg = VGG16()
+
+model_iris_lr = None
+model_iris_svm = None
+model_iris_dt = None
+model_iris_deep = None
+def load_iris():
+    global model_iris_lr, model_iris_svm, model_iris_dt, model_iris_deep
+    model_iris_lr = joblib.load(os.path.join(app.root_path, 'model/iris_lr.pkl'))
+    model_iris_svm = joblib.load(os.path.join(app.root_path, 'model/iris_svm.pkl'))
+    model_iris_dt = joblib.load(os.path.join(app.root_path, 'model/iris_dt.pkl'))
+    model_iris_deep = load_model(os.path.join(app.root_path, 'model/iris_deep.hdf5'))
 
 @app.route('/')
 def index():
     menu = {'home':True, 'rgrs':False, 'stmt':False, 'clsf':False, 'clst':False, 'user':False}
     return render_template('home.html', menu=menu)
 
-@app.route('/regression', methods=['GET','POST'])
+@app.route('/regression', methods=['GET', 'POST'])
 def regression():
     menu = {'home':False, 'rgrs':True, 'stmt':False, 'clsf':False, 'clst':False, 'user':False}
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('regression.html', menu=menu)
     else:
-        spe_names = ['Setosa', 'Versicolor', 'Virginica']
-        slen = float(request.form['slen'])
-        plen = float(request.form['plen'])
-        pwid = float(request.form['pwid'])    
-        spe = int(request.form['species'])
-        species = spe_names[spe]
-        swid = 0.63711424 * slen - 0.53485016 * plen + 0.55807355 * pwid - 0.12647156 * spe + 0.78264901
+        sp_names = ['Setosa', 'Versicolor', 'Virginica']
+        slen = float(request.form['slen'])      # Sepal Length
+        plen = float(request.form['plen'])      # Petal Length
+        pwid = float(request.form['pwid'])      # Petal Width
+        sp = int(request.form['species'])       # Species
+        species = sp_names[sp]
+        swid = 0.63711424 * slen - 0.53485016 * plen + 0.55807355 * pwid - 0.12647156 * sp + 0.78264901
         swid = round(swid, 4)
         iris = {'slen':slen, 'swid':swid, 'plen':plen, 'pwid':pwid, 'species':species}
-        return render_template('regression_result.html', menu=menu, iris=iris)
-@app.route('/sentiment')
-def sentiment():
-    pass
+        return render_template('reg_result.html', menu=menu, iris=iris)
 
-@app.route('/classification')
+@app.route('/sentiment', methods=['GET', 'POST'])
+def sentiment():
+    menu = {'home':False, 'rgrs':False, 'stmt':True, 'clsf':False, 'clst':False, 'user':False}
+    if request.method == 'GET':
+        return render_template('sentiment.html', menu=menu)
+    else:
+        res_str = ['부정', '긍정']
+        review = request.form['review']
+        # LR 처리
+        review_lr = re.sub(r"\d+", " ", review)
+        review_lr_dtm = movie_lr_dtm.transform([review_lr]) 
+        result_lr = res_str[movie_lr.predict(review_lr_dtm)[0]]
+        # NB 처리
+        review_nb = nb_transform(review)
+        review_nb_dtm = movie_nb_dtm.transform([review_nb])
+        result_nb = res_str[movie_nb.predict(review_nb_dtm)[0]]
+        #결과처리
+        movie = {'review': review, 'result_lr': result_lr, 'result_nb': result_nb}
+        return render_template('senti_result.html', menu=menu, movie=movie)
+
+@app.route('/classification', methods=['GET', 'POST'])
 def classification():
-    pass
+    menu = {'home':False, 'rgrs':False, 'stmt':False, 'clsf':True, 'clst':False, 'user':False}
+    if request.method == 'GET':
+        return render_template('/classification.html', menu=menu)
+    else:
+        f = request.files['image']
+        filename = os.path.join(app.root_path, 'static/images/uploads/') + secure_filename(f.filename)
+        f.save(filename)
+        img = np.array(Image.open(filename).resize((224, 224)))
+        yhat = vgg.predict(img.reshape(-1, 224, 224, 3))
+        label_key = np.argmax(yhat)
+        label = decode_predictions(yhat)
+        label = label[0][0]
+        return render_template('cla_result.html', menu=menu, 
+                                filename=secure_filename(f.filename), 
+                                label=label[1], pct='%.2f' % (label[2]*100))
+
+@app.route('/classification_iris', methods=['GET', 'POST'])
+def classification_iris():
+    menu = {'home':False, 'rgrs':False, 'stmt':False, 'clsf':True, 'clst':False, 'user':False}
+    if request.method == 'GET':
+        return render_template('classification_iris.html', menu=menu)
+    else:
+        sp_names = ['Setosa', 'Versicolor', 'Virginica']
+        slen = float(request.form['slen'])      # Sepal Length
+        swid = float(request.form['swid'])      # Sepal Width
+        plen = float(request.form['plen'])      # Petal Length
+        pwid = float(request.form['pwid'])      # Petal Width
+        test_data = np.array([slen, swid, plen, pwid]).reshape(1,4)
+        species_lr = sp_names[model_iris_lr.predict(test_data)[0]]
+        species_svm = sp_names[model_iris_svm.predict(test_data)[0]]
+        species_dt = sp_names[model_iris_dt.predict(test_data)[0]]
+        species_deep = sp_names[model_iris_deep.predict_classes(test_data)[0]]
+        iris = {'slen':slen, 'swid':swid, 'plen':plen, 'pwid':pwid, 
+                'species_lr':species_lr, 'species_svm':species_svm,
+                'species_dt':species_dt, 'species_deep':species_deep}
+        return render_template('cla_iris_result.html', menu=menu, iris=iris)
 
 @app.route('/clustering')
 def clustering():
     pass
 
+@app.route('/KMJ')
+def user_kmj():
+    menu = {'home':False, 'rgrs':False, 'stmt':False, 'clsf':False, 'clst':False, 'user':True}
+    return render_template('user/KMJ.html', menu=menu)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    load_movie_lr()
+    load_movie_nb()
+    load_vgg()
+    load_iris()
+    app.run()
